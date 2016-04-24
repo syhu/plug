5205f89c7edb6eb6733540d323c544f9cff71038
Shun-Yun Hu <syhu@imonology.com>
2016-04-25 01:02:04 +0800

    ADD: publish/subscribe API calls

--- lobby/logic.js
+++ lobby/logic.js
@@ -1,14 +1,46 @@
 
 
-IC.API.add('HELLO_EVENT', function (args, onDone) {
-	LOG.warn('HELLO_WORLD called');
+IC.API.add('HELLO_EVENT', function (args, onDone, conn) {
 
-	var err;
+	LOG.warn(conn);
+	
 	if (!args.name) {
-		err = 'no name provided';
+		var err = 'no name provided';
+		return onDone(err);
 	}
-		
-	UTIL.safeCall(onDone, err, (err ? undefined : {hello: args.name}));
+
+	onDone(null, {hello: args.name});
 });
 
 
+
+
+IC.API.add('SUBSCRIBE', function (args, onDone) {
+
+	// record the subscription
+	// TODO: need access to the connection object calling this method
+	LOG.warn('subscribing: ');
+	LOG.warn(args);
+	
+	onDone(null, {msg: 'subscribe success!'});
+	
+}, {
+	center: 'array',
+	radius: '+number'		// radius is optional
+});
+
+
+IC.API.add('PUBLISH', function (args, onDone) {
+
+	// record the subscription
+	// TODO: need access to the connection object calling this method
+	LOG.warn('publishing: ');
+	LOG.warn(args);
+	
+	onDone(null, {msg: 'publish success!'});
+	
+}, {
+	data:	'object',
+	center: '+array',
+	radius: '+number'		// radius is optional
+});
